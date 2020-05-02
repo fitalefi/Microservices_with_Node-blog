16a81ec76a8b548ba5067089d23e1681a1d61e8f
efi fital <fitalefi@gmail.com>
2020-05-02 18:13:48 +0300

    event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyPareser = require('body-parser');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
